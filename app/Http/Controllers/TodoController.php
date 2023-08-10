60923d233540191f2bf7b012114a9aef41fbe750
dhoni <dhoni@kanosolution.com>
2023-08-11 00:37:38 +0700

    fix

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -27,29 +27,7 @@ class TodoController extends Controller
         $data = Todo::create($req);
         return response()->json(['status' => true, 'data' => $data], 201);
     }
-
-    // public function update(Request $request, $id): JsonResponse
-    // {
-    //     $validateUser = Validator::make(
-    //         $request->all(),
-    //         [
-    //             'has_completed' => 'required',
-    //         ]
-    //     );
-
-    //     if ($validateUser->fails()) {
-    //         return response()->json([
-    //             'status' => false,
-    //             'message' => 'validation error',
-    //             'errors' => $validateUser->errors()
-    //         ], 401);
-    //     }
-
-    //     $data = Todo::find($id);
-    //     $data->has_completed = $request->has_completed;
-    //     $data->update();
-    //     return response()->json(['status' => true, 'data' => $data], 202);
-    // }
+    
     public function update(Request $request, $id): JsonResponse
     {
         $data = Todo::find($id);

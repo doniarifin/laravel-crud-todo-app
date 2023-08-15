b4d6f430d3c787366d7adbc0c004bd4c4430b492
dhoni <dhoni@kanosolution.com>
2023-08-15 10:01:31 +0700

    fix path

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -10,7 +10,7 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            redirect: '/login'
+            redirect: '/home'
         },
         {
             path: '/login',
